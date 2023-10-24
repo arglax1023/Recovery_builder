e130a1caf1fec2069ebee421bfdb2193e33c37e7
arglax1023 <131213056+arglax1023@users.noreply.github.com>
2023-10-24 18:28:20 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,11 +6,11 @@ export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp
 export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
+export DEVICE="rhodeñ" # codename used in device tree
+export DT_LINK="https://github.com/twrpdtgen/android_device_motorola_rhodep/tree/rhodep_g-user-11-S1SUS32.73-112-22-2-59224d-release-keys" # device tree link
 export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
-export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
+export VENDOR="motorola" # device manufacturer or vendor
+export TARGET="bootimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
